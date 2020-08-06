368960a112f70dc5b16a1db05f8b42f84764d732
Bezzmozzg <38427951+Bezzmozzg@users.noreply.github.com>
2020-08-06 23:10:03 +0500

    ~~~

---
+++ Main.py
@@ -0,0 +1,58 @@
+import psycopg2
+from API_interaction import GetMeets
+
+token = "plcOgcreJML2O35Xv0Z6fn3zPMRFtYmX5RzxlXfW5rMVPAIRVrbp9dvWvA843aqr"
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                  "AppleWebKit/537.36 (KHTML, like Gecko)"
+                  "Chrome/84.0.4147.89"
+                  "Safari/537.36"}
+gm = GetMeets(token, headers)
+
+db_1 = {
+    'dbname': 'd6qfh4tmk9h4t2',
+    'user': 'xqhkevexwktfkk',
+    'password': 'cb8c1a6070ebed38248446e0960ffe7b3fd8ea4927c0aa3ef4d7e1b630588b3f',
+    'host': 'ec2-34-225-162-157.compute-1.amazonaws.com'
+}
+
+db_2 = {
+    'dbname': 'test',
+    'user': 'postgres',
+    'password': '123',
+    'host': 'localhost'
+}
+
+with psycopg2.connect(dbname=db_2['dbname'],
+                      user=db_2['user'],
+                      password=db_2['password'],
+                      host=db_2['host']) as conn:
+
+    with conn.cursor() as cursor:
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS b_khalutornykh (
+                Id SERIAL PRIMARY KEY,
+                Date DATE,
+                Bizdev character varying,
+                Company character varying,
+                Category character varying);
+            """
+        )
+        conn.commit()
+        for day in gm.get_days():
+            for meets in gm.get_meets(day):
+                cursor.execute(
+                    """
+                    SELECT * FROM b_khalutornykh WHERE ("date", "bizdev", "company", category)=('{}', '{}', '{}', '{}')
+                    """.format(meets['date'], meets['bizdev'], meets['company'], meets['category'])
+                )
+                print(meets)
+                if not cursor.fetchall():
+                    print(123)
+                    cursor.execute(
+                        """
+                        INSERT INTO b_khalutornykh (date, bizdev, company, category) VALUES ('{}', '{}', '{}', '{}')
+                        """.format(meets['date'], meets['bizdev'], meets['company'], meets['category'])
+                    )
+            conn.commit()
